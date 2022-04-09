83794902e5545c8d7b329003482b6fe52693cf09
sumit-crubn <sumit.patel@crubn.com>
2022-04-09 12:40:28 +0530

    smart contract

--- blockchain/contracts/Migrations.sol
+++ blockchain/contracts/Migrations.sol
@@ -1,19 +1,56 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.3;
 
-contract Migrations {
-  address public owner = msg.sender;
-  uint public last_completed_migration;
 
-  modifier restricted() {
-    require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner"
-    );
+contract DAOContract {
+
+ address owner;
+
+ struct userDetails {
+     address userAddress;
+     string userName;
+     string userRegistrationTimestamp;
+     uint256 userId;
+     uint256 userCreditScore;
+ }
+
+ userDetails[] public users;
+
+ mapping ( address => userDetails) public userDetailsMapping;
+ mapping ( string => address ) public getAddressFromName;
+ 
+ modifier onlyOwner(){
+    require(msg.sender == owner);
     _;
-  }
+}
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
+function registerUser(address _userAddress, string memory _userName, string memory _userRegistrationTimestamp, uint256 _userId, uint256 _userCreditScore) public {
+    userDetails memory x = userDetails(
+        _userAddress,
+        _userName,
+        _userRegistrationTimestamp,
+        _userId,
+        _userCreditScore
+    );
+
+    users.push(x);
+    userDetailsMapping[msg.sender] = x;
+    getAddressFromName[_userName] = msg.sender;
+
+}
+
+function fetchNumberOfUsers() public view returns (uint256) {
+   return users.length;
+}
+
+function fetchUserByName(string memory _userName)  public view returns (userDetails memory ){
+    address contract_addr = getAddressFromName[_userName];
+    return userDetailsMapping[contract_addr];
+}
+
+// function requestFund(uint256 _userId, uint256 _requestedAmount )  public onlyOwner {
+//     //find user
+//     require( userDetails.userRegistrationTimestamp > 1*year );
+//     require ( userDetails.userCreditScore >= 750 );
+//     //transfer amount to the requested user address
+//  }
 }
