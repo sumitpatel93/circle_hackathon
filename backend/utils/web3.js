76629ca2a50a228dc6feec3333bac9825a16fca4
sumit-crubn <sumit.patel@crubn.com>
2022-04-13 12:57:23 +0530

    Update contract from polygon to rinkeby Add sendsigned transaction data to eth

--- backend/utils/web3.js
+++ backend/utils/web3.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const cors = require('cors');
 const express = require('express');
-const Tx = require('ethereumjs-tx')
+const Tx = require('ethereumjs-tx').Transaction;
 const Key = require('../config/keys')
 var Web3 = require("web3");
 const app = express();
@@ -16,7 +16,7 @@ var web3;
 if (typeof web3 !== "undefined") {
   web3 = new Web3(web3.currentProvider);
 } else {
-  web3 = new Web3(new Web3.providers.HttpProvider("https://rpc-mumbai.maticvigil.com/"));
+  web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/0f333401437149e28c3696b36eb02f93"));
 
 }
 
@@ -25,52 +25,40 @@ web3.eth.getBlockNumber().then(console.log);
 web3.eth.net.getNetworkType()
   .then(console.log);
 
-const sendSignedTransaction = (senderPrivateKey, res, next) =>{
+const sendSignedTransaction = async () =>{
 
-    const userPvtKey = req.body.senderPrivateKey
+    const userPvtKey = '0xb3021fb06b6396f628dda47d81701150e7d241476ebfa40fa6e919e61e294f45'
     const pvtKey = userPvtKey.substring(2)
     var privateKey = new Buffer(pvtKey, 'hex');
     var contractInstance = new web3.eth.Contract(
         Key.interface,
         Key.contractAddress
       );
-    var encodedABI = contractInstance.methods.saveWatchDetails(
-        ownerName,
-        watchInsured,
-        dateOfLoss,
-        cityOfLoss,
-        
+    var encodedABI = contractInstance.methods.registerUser(
+        "0x655e5cB1F1EABE2767EFEd4E90714D2A92608d15",
+        "userName",
+        "121212",
+        12321,
+        123
     ).encodeABI()
     const nonce = await web3.eth.getTransactionCount("0x655e5cB1F1EABE2767EFEd4E90714D2A92608d15");
+    console.log('nonce--', nonce);
     var rawTx = {
         nonce: web3.utils.toHex(nonce),
         from: "0x655e5cB1F1EABE2767EFEd4E90714D2A92608d15",
         to:   Key.contractAddress,
         gasLimit: '0x3d0900',
         gasPrice: web3.utils.toHex(web3.utils.toWei('30', 'gwei')),
-        chainId: web3.utils.toHex(80001),
+        chainId: '0x04',
         data: encodedABI,
       };
   
-      var tx = new Tx(rawTx, { 'chain': 'Mumbai Testnet' });
+      var tx = new Tx(rawTx, {chain: 'rinkeby'});
       tx.sign(privateKey);
       var serializedTx = tx.serialize();
   
-      web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'), function (err, hash) {
-        console.log(err)
-        if (err) {
-          res.status(500).json({
-            Body: err
-          })
-        }
-        else {
-          res.status(200).json({
-            status: true,
-            message: "Action Completed Successfully",
-            data: hash
-          })
-        }
-      });
+      web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+      .on('receipt', console.log);
 }
 
 
@@ -78,5 +66,5 @@ const fetchContractData = (req, res, next) => {
 
 }
 
-
-module.exports = { sendSignedTransaction, fetchContractData };
+sendSignedTransaction()
+//module.exports = { sendSignedTransaction, fetchContractData };
