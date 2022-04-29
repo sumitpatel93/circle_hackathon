a75351122d0ca67973c4dbf9a4d290a735f46564
Sidhanti Patil <sidpatil2209@gmail.com>
2022-04-29 23:34:14 +0530

    findUser request

--- frontend/pages/dashboard/[user].tsx
+++ frontend/pages/dashboard/[user].tsx
@@ -11,6 +11,8 @@ const Dashboard = () => {
     const [loan, setLoan] = useState(0);
     const [address, setAddress] = useState("");
     const [amount,setAmount] = useState("");
+    const [created, setCreated] = useState("");
+    const [pvtKey, setPvtKey] = useState("");
 
     const getUserDetails = async () =>{
       const res = await fetch("http://localhost:3010/circleHackathon/userData", {
@@ -25,13 +27,33 @@ const Dashboard = () => {
         const resJson = await res.json()
         setCreditScore(resJson.Body.userCreditScore);
         setLoan(resJson.Body.loanAmount)
-        // setLoan(220);
+        setLoan(220);
         setAddress(resJson.Body.userAddress)
+      }
+      const findUser =async() =>{
+        const res = await fetch("http://localhost:3010/circleHackathon/findUser", {
+            method: "POST",
+            body: JSON.stringify({
+              email : username
+            }),
+            headers:{
+              'Content-Type': 'application/json'
+            }
+          });
+          const resJson = await res.json();
+          setCreated(resJson.Body.createdAt);
+          setPvtKey(resJson.Body.pvtKey);
+          console.log(resJson);
       }
 
       useEffect(() => {
-        getUserDetails()
+        getUserDetails();
+        findUser();
       }, [])
+  
+  
+
+      
 
 
     const handleSubmit =async(e: any) =>{
@@ -39,7 +61,7 @@ const Dashboard = () => {
       const res = await fetch("http://localhost:3010/circleHackathon/requestFund", {
           method: "POST",
           body: JSON.stringify({
-            userPrivateKey : "0x177aa6b4e3af0e0923ac3f600e30b28fa20e172e1e4f081e9d2314c2cc7a1f10",
+            userPrivateKey : pvtKey,
             userAddress : address,
             userName : username,
             amount: amount,
@@ -51,12 +73,13 @@ const Dashboard = () => {
         const resJson = await res.json();
         console.log(resJson);
     }
+    
 
     const deposit =async() =>{
       const res = await fetch("http://localhost:3010/circleHackathon/deposit", {
           method: "POST",
           body: JSON.stringify({
-            userPrivateKey : "0x177aa6b4e3af0e0923ac3f600e30b28fa20e172e1e4f081e9d2314c2cc7a1f10",
+            userPrivateKey : pvtKey,
             userAddress : address,
             userName : username,
             amount: loan,
@@ -69,8 +92,6 @@ const Dashboard = () => {
         console.log(resJson);
     }
 
-
-    
 
     return(
     <div className="flex text-white font-thin bg-black min-h-screen flex-col">
@@ -107,11 +128,10 @@ const Dashboard = () => {
 
           <div className="flex p-3 text-xl">
             <p className="font-semibold mr-5">Account creation date: </p>
-            <p></p>
+            <p>{created}</p>
           </div>
             
           <form>
-
             <div className="flex p-3 text-xl">
               <p className="font-semibold mr-5">Enter loan amount to be taken: </p>
               <p><input type="number" value={amount} className="text-black m-1 rounded p-0.5" onChange={(e) => setAmount(e.target.value)}/> USDC</p>
@@ -120,8 +140,6 @@ const Dashboard = () => {
                 Submit
             </button>
           </form>
-            
-            
         </div>
         
       </div>
